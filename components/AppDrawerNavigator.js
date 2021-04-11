4a10ba977b2843ac419e56b0ad9fd7692955553b
anjalikapur2020 <70400894+anjalikapur2020@users.noreply.github.com>
2021-04-11 19:17:19 +0530

    Update AppDrawerNavigator.js

--- components/AppDrawerNavigator.js
+++ components/AppDrawerNavigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 import { AppTabNavigator } from './AppTabNavigator'
 import CustomSideBarMenu  from './CustomSideBarMenu';
-import MySelectionScreen from '../screens/MySelectionScreen';
+import MySelectionScreen from '../screens/MyselectionScreen';
 import NotificationScreen from '../screens/NotificationScreen';
 import SettingScreen from '../screens/SettingScreen';
 import MyReceivedBooksScreen from '../screens/MyReceivedBooksScreen';
